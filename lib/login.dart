e85db3cf829f4ce1a2a06097bd882095055f58ba
Intan Yoshana <intan318@gmail.com>
2019-08-24 15:06:28 +0700

    added validation
    validation for email and password

--- lib/login.dart
+++ lib/login.dart
@@ -6,9 +6,18 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool validasi = false;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Form(key: _formKey, autovalidate: validasi, child: FormLogin()));
+  }
 
+  Widget FormLogin() {
     return Column(
       children: <Widget>[
         Padding(
@@ -23,9 +32,14 @@ class _LoginPageState extends State<LoginPage> {
         Padding(
             padding: const EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
             child: TextFormField(
+                validator: validasiEmail,
+                onSaved: (String em) {
+                  email = em;
+                },
                 keyboardType: TextInputType.emailAddress,
                 autofocus: false,
                 decoration: InputDecoration(
+                    labelText: 'E-mail',
                     hintText: 'E-mail',
                     suffixIcon: Icon(Icons.person, color: Colors.blue),
                     enabledBorder: OutlineInputBorder(
@@ -35,6 +49,10 @@ class _LoginPageState extends State<LoginPage> {
         Padding(
             padding: const EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
             child: TextFormField(
+                validator: validasiPassword,
+                onSaved: (String p) {
+                  password = p;
+                },
                 obscureText: true,
                 autofocus: false,
                 decoration: InputDecoration(
@@ -46,31 +64,67 @@ class _LoginPageState extends State<LoginPage> {
                     )))),
         Padding(
           padding: EdgeInsets.symmetric(vertical: 16.0),
-          child: SizedBox(
-            width: double.infinity,
-            height: 24,
-            child: RaisedButton(
-            color: Colors.blue,
-            child: Text(
-              "Log In",
-              style: TextStyle(
-                  fontSize: 20.0, fontWeight: FontWeight.w400, color: Colors.black),
+          child: Material(
+            borderRadius: BorderRadius.circular(30.0),
+            shadowColor: Colors.lightBlueAccent.shade100,
+            elevation: 8.0,
+            child: MaterialButton(
+              minWidth: 200.0,
+              height: 42.0,
+              onPressed: () {
+                _validasiInputan();
+              },
+              color: Colors.lightBlueAccent,
+              child: Text('Log In', style: TextStyle(color: Colors.black)),
             ),
-            onPressed: () {},
-          )),
+          ),
         ),
-
         Padding(
-          padding: const EdgeInsets.only(top:8),
+          padding: const EdgeInsets.only(top: 8),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text("Register account", style: TextStyle(color: Colors.blue, fontSize: 16.0, fontWeight: FontWeight.w300)),
-              Text("Forgot password", style: TextStyle(color: Colors.blue, fontSize: 16.0, fontWeight: FontWeight.w200))
+              Text("Register account",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w300)),
+              Text("Forgot password",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w300))
             ],
           ),
         )
       ],
     );
+  }
+
+  String validasiEmail(String email) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(email))
+      return 'Enter Valid Email';
+    else
+      return null;
+  }
+
+  String validasiPassword(String pass) {
+    if (pass.length < 8)
+      return 'Password must be at least 8 characters';
+    else
+      return null;
+  }
+
+  void _validasiInputan() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+    } else {
+      setState(() {
+        validasi = true;
+      });
+    }
   }
 }
